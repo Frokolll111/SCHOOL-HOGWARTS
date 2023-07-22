03dac0f3b581ed86655b0cefd807eca82301ffcc
Frokolll111 <buther1433@gmail.com>
2023-07-22 11:12:28 +0700

    Сделал дз

---
+++ service/FacultyService.java
@@ -0,0 +1,44 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+@Service
+public class FacultyService {
+    private final HashMap<Long, Faculty> faculties = new HashMap<Long, Faculty>();
+    private long idFaculty = 0;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculty.setId(++idFaculty);
+        faculties.put(idFaculty, faculty);
+        return faculty;
+    }
+
+    public Faculty readFaculty(long idFaculty) {
+        return faculties.get(idFaculty);
+    }
+
+    public Faculty updateFaculty(Faculty faculty) {
+        if (faculties.containsKey(idFaculty)) {
+            faculties.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+
+    public Faculty deleteFaculty(long idFaculty) {
+        return faculties.remove(idFaculty);
+    }
+
+    public Collection<Faculty> allFaculty() {
+        return faculties.values();
+    }
+    public Collection<Faculty> colorFilterFaculty(String color) {
+        return faculties.values().stream()
+                .filter(faculty -> faculty.getColor() == color)
+                .collect(Collectors.toList());
+    }
+}
---
+++ service/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+@Service
+public class StudentService {
+    private final HashMap<Long, Student> students = new HashMap<>();
+    private  long idStudent = 0;
+
+    public Student createStudent(Student student) {
+        student.setId(++idStudent);
+        students.put(idStudent,student);
+        return student;
+    }
+
+    public Student readStudent(long idStudent) {
+        return students.get(idStudent);
+    }
+
+    public Student updateStudent(Student student) {
+        if (students.containsKey(student.getId())) {
+            students.put(student.getId(), student);
+            return student;
+        }
+        return null;
+    }
+
+    public Student deleteStudent(long idStudent) {
+        return students.remove(idStudent);
+    }
+
+    public Collection<Student> allStudent() {
+        return students.values();
+    }
+
+    public Collection ageFilterStudent(int age) {
+        return students.values().stream()
+                .filter(students -> students.getAge() == age)
+                .collect(Collectors.toList());
+    }
+}
